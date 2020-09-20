7fd6d6d31aae4b2ee93fcf9f8e84bc523bbc06c8
Bo Biene <bo_biene@hotmail.com>
2020-09-20 12:35:09 +0200

    remove deamon script

--- SolaxGateway.py
+++ SolaxGateway.py
@@ -34,26 +34,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                 self.end_headers()
                 self.wfile.write(dataAsJson.encode('utf-8'))
 
-
-class SolaxDaemon(daemon):
-        def run(self):               
-                print("Starting server...")
-                httpd = HTTPServer(('', 80), SimpleHTTPRequestHandler)
-                httpd.serve_forever()
-
-if __name__ == "__main__":
-        solaxDaemon = SolaxDaemon('/tmp/SolaxGateway.pid')
-        if len(sys.argv) == 2:
-                if 'start' == sys.argv[1]:
-                        solaxDaemon.start()
-                elif 'stop' == sys.argv[1]:
-                        solaxDaemon.stop()
-                elif 'restart' == sys.argv[1]:
-                        solaxDaemon.restart()
-                else:
-                        print( "Unknown command" )
-                        sys.exit(2)
-                sys.exit(0)
-        else:
-                print ( "usage: %s start|stop|restart" % sys.argv[0] )
-                sys.exit(2)
+    
+print("Starting server...")
+httpd = HTTPServer(('', 80), SimpleHTTPRequestHandler)
+httpd.serve_forever()
