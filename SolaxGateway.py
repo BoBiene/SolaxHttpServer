6e3156d0cba05b3398e4c97bcb7fc8a213b6de8e
Bo Biene <bo_biene@hotmail.com>
2020-09-23 19:15:52 +0200

    Offline Message

--- SolaxGateway.py
+++ SolaxGateway.py
@@ -11,7 +11,34 @@ async def LoadSolaxData():
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-
+    OFFLINE_MESSAGE = """{
+  "PV1 Current": NaN,
+  "PV2 Current": NaN,
+  "PV1 Voltage": NaN,
+  "PV2 Voltage": NaN,
+  "Output Current": NaN,
+  "Network Voltage": NaN,
+  "AC Power": NaN,
+  "Inverter Temperature": NaN,
+  "Today's Energy": NaN,
+  "Total Energy": NaN,
+  "Exported Power": NaN,
+  "PV1 Power": NaN,
+  "PV2 Power": NaN,
+  "Battery Voltage": NaN,
+  "Battery Current": NaN,
+  "Battery Power": NaN,
+  "Battery Temperature": NaN,
+  "Battery Remaining Capacity": NaN,
+  "Total Feed-in Energy": NaN,
+  "Total Consumption": NaN,
+  "Power Now": NaN,
+  "Grid Frequency": NaN,
+  "EPS Voltage": NaN,
+  "EPS Current": NaN,
+  "EPS Power": NaN,
+  "EPS Frequency": NaN
+}"""
     def do_GET(self):
         if self.path == "/favicon.ico":
             self.send_response(404)
@@ -23,8 +50,9 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             if timedout:
                 [task.cancel() for task in timedout]
                 loop.run_until_complete(asyncio.wait(timedout))
-                self.send_response(408)
+                self.send_response(200)
                 self.end_headers()
+                self.wfile.write(OFFLINE_MESSAGE.encode.encode('utf-8'))
             else:
                 responseTask = responseTasks.pop()
                 response = responseTask.result()
